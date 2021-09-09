9098101b04a6a13e5cc10a72473967235e8f44b8
Marina Areias <marinasareias@gmail.com>
2021-09-09 21:34:49 +0200

    fixed route error

--- app.js
+++ app.js
@@ -20,13 +20,18 @@ require("./config")(app);
 
 // default value for title local
 const projectName = "cleanser";
-const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
 app.use("/", index);
+const authRoutes = require("./routes/auth.routes");
+app.use("/", authRoutes);
+// const profileRoutes = require("./routes/profile.routes");
+// app.use("/", profileRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
