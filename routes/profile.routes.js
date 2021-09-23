3eddecd2d751f1fd4a3a4b91c28f0fa3f2893998
whatkimkong <whatkimkong@gmail.com>
2021-09-24 00:55:24 +0200

    css dashboard & library, user functionality

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
+const UserModel = require("../models/User.model");
 
+router.get("/settings", (req, res, next) => {
+      res.render("profile/settings.hbs"); // find the information of this current user !! <3 
+});
 
 module.exports = router;
 
